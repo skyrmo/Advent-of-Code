591c28b626756c3d5c615b4e0fa0fab991e904b3
Skyrmo <owaingskyrme@gmail.com>
2026-01-17 21:11:08 +0800

    Completed Day 6, Parts 1 & 2.

--- day_06/day_06_part_1.py
+++ day_06/day_06_part_1.py
@@ -1,9 +1,9 @@
-
 import os
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
@@ -18,15 +18,58 @@ def parse_input(file_path):
 
         return data
 
+
+def redist(memory, max_idx, n):
+    prev_value = memory[max_idx]
+    memory[max_idx] = 0
+
+    increase_amount = max(1, prev_value // n)
+    cur_idx = (max_idx + 1) % n
+
+    while prev_value > 0:
+        memory[cur_idx] += increase_amount
+        prev_value -= increase_amount
+        cur_idx = (cur_idx + 1) % n
+
+    return memory
+
+
+def find_max(memory):
+    max_idx = 0
+    max_val = 0
+    for i, value in enumerate(memory):
+        if value > max_val:
+            max_val = value
+            max_idx = i
+
+    return max_idx
+
+
 def solve(input_data):
-    print(input_data)
+    memory = [int(x) for x in input_data.split("\t")]
+    n = len(memory)
+
+    seen_states = set()
+    steps = 0
+
+    while str(memory) not in seen_states:
+        seen_states.add(str(memory))
+
+        max_idx = find_max(memory)
+
+        redist(memory, max_idx, n)
+
+        steps += 1
+
+    return steps
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    input_path = os.path.join(script_dir, 'input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
     # input_path = os.path.join(script_dir, 'sample_input.txt')
 
     # Parse input
@@ -36,5 +79,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 06, Part One: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
