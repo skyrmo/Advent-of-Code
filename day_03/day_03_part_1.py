d2f22baf5b5c9ea0ab0563e4bd6386b0c94fdb0e
Skyrmo <owaingskyrme@gmail.com>
2026-01-17 17:53:55 +0800

    Completed Day 3, parts 1 & 2.

--- day_03/day_03_part_1.py
+++ day_03/day_03_part_1.py
@@ -1,9 +1,10 @@
-
+import math
 import os
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
@@ -18,15 +19,26 @@ def parse_input(file_path):
 
         return data
 
+
 def solve(input_data):
-    print(input_data)
+    tgt = int(input_data)
+    ring = math.ceil((math.sqrt(tgt) - 1) / 2)
+    side_len = (2 * ring) + 1
+    ring_max_val = side_len**2
+    offset = ring_max_val - tgt
+
+    side_position = offset % (2 * ring)
+    offset_from_center = abs(side_position - ring)
+
+    return ring + offset_from_center
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    input_path = os.path.join(script_dir, 'input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
     # input_path = os.path.join(script_dir, 'sample_input.txt')
 
     # Parse input
@@ -36,5 +48,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 03, Part One: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- day_03/day_03_part_2.py
+++ day_03/day_03_part_2.py
@@ -1,9 +1,10 @@
-
+import math
 import os
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
@@ -18,15 +19,49 @@ def parse_input(file_path):
 
         return data
 
+
+def check_nbrs(pos, grid) -> int:
+    r, c = pos
+    dirs = [
+        (r - 1, c - 1),
+        (r - 1, c),
+        (r - 1, c + 1),
+        (r, c - 1),
+        (r, c + 1),
+        (r + 1, c - 1),
+        (r + 1, c),
+        (r + 1, c + 1),
+    ]
+    return sum(grid.get((nr, nc), 0) for nr, nc in dirs)
+
+
 def solve(input_data):
-    print(input_data)
+    grid = {(0, 0): 1}
+
+    dir = 0
+    dirs = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+    r, c = 0, 0
+
+    while grid[(r, c)] <= int(input_data):
+        r = r + dirs[dir][0]
+        c = c + dirs[dir][1]
+
+        grid[(r, c)] = check_nbrs((r, c), grid)
+
+        directions = [(r - 1, c), (r, c + 1), (r + 1, c), (r, c - 1)]
+        total_nbrs = sum(1 if (nr, nc) in grid else 0 for nr, nc in directions)
+        if total_nbrs == 1:
+            dir = (dir + 1) % 4
+
+    return grid[(r, c)]
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    input_path = os.path.join(script_dir, 'input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
     # input_path = os.path.join(script_dir, 'sample_input.txt')
 
     # Parse input
@@ -36,5 +71,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 03, Part Two: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
