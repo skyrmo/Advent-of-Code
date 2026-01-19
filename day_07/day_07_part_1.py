6114311da18468131e7408592f746480a4d133b5
Skyrmo <owaingskyrme@gmail.com>
2026-01-19 19:04:58 +0800

    Completed Day 7, parts 1 & 2.

--- day_07/day_07_part_1.py
+++ day_07/day_07_part_1.py
@@ -1,14 +1,15 @@
-
 import os
+from collections import defaultdict
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
         # 2. Read as a list of lines
-        # return data.split('\n')
+        return data.split("\n")
 
         # 3. Read as a list of integers
         # return [int(line) for line in data.split('\n')]
@@ -18,15 +19,53 @@ def parse_input(file_path):
 
         return data
 
+
+# class Node:
+#     def __init__(self, name, weight, children=[]):
+#         self.name = name
+#         self.weight = int(weight)
+#         self.children = children
+
+#     def __repr__(self):
+#         return f"Node({self.name}, {self.weight}, {self.children})"
+
+
 def solve(input_data):
-    print(input_data)
+    in_degrees = defaultdict(int)
+    for line in input_data:
+        node, children = None, None
+        if "->" in line:
+            node, children = line.split("->")
+            children = children.strip().split(", ")
+        else:
+            node = line.strip()
+            # print(node, children)
+
+        name, weight = node.strip().split(" ")
+        weight = int(weight[1:-1].strip())
+        # print(line, "---------------")
+        # print(name, weight, children)
+
+        if children:
+            if name not in in_degrees:
+                in_degrees[name] = 0
+
+            for child in children:
+                in_degrees[child] += 1
+
+    # for k, v in in_degrees.items():
+    # print(k, v)
+
+    # print([k for k, v in in_degrees.items() if v == 0])
+    return [k for k, v in in_degrees.items() if v == 0][0]
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    input_path = os.path.join(script_dir, 'input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
     # input_path = os.path.join(script_dir, 'sample_input.txt')
 
     # Parse input
@@ -36,5 +75,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 07, Part One: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- day_07/day_07_part_2.py
+++ day_07/day_07_part_2.py
@@ -1,14 +1,15 @@
-
 import os
+from collections import defaultdict, deque
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
         # 2. Read as a list of lines
-        # return data.split('\n')
+        return data.split("\n")
 
         # 3. Read as a list of integers
         # return [int(line) for line in data.split('\n')]
@@ -18,15 +19,54 @@ def parse_input(file_path):
 
         return data
 
+
+weights = defaultdict(int)
+adj = defaultdict(list)
+
+
+def dfs(node):
+    weight = weights[node]
+
+    child_weights = [(child, dfs(child)) for child in adj[node]]
+    child_weights_equal = all([w == child_weights[0][1] for c, w in child_weights])
+
+    if child_weights_equal:
+        weight += sum(w for c, w in child_weights)
+    else:
+        print("----")
+        for c, w in child_weights:
+            print(c, w, weights[c])
+        print("----")
+
+    return weight
+
+
 def solve(input_data):
-    print(input_data)
+    for line in input_data:
+        node = line.split(")")
+        name, weight = node[0].strip().split("(")
+        weights[name.strip()] = int(weight)
+
+    for line in input_data:
+        if "->" in line:
+            node, children = line.split("->")
+            children = children.strip().split(", ")
+            name, _ = node.split(" (")
+            name = name.strip()
+
+            if children:
+                for child in children:
+                    adj[name].append(child.strip())
+
+    return dfs("ahnofa")
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    input_path = os.path.join(script_dir, 'input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
     # input_path = os.path.join(script_dir, 'sample_input.txt')
 
     # Parse input
@@ -36,5 +76,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 07, Part Two: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
