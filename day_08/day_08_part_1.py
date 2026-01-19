995c2ac92d8b32a7982581254353d2c10ba341f2
Skyrmo <owaingskyrme@gmail.com>
2026-01-19 19:44:03 +0800

    Completed Day 8, parts 1 & 2.

--- day_08/day_08_part_1.py
+++ day_08/day_08_part_1.py
@@ -1,14 +1,16 @@
-
 import os
+import re
+from collections import defaultdict
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
         # 2. Read as a list of lines
-        # return data.split('\n')
+        return data.split("\n")
 
         # 3. Read as a list of integers
         # return [int(line) for line in data.split('\n')]
@@ -18,16 +20,66 @@ def parse_input(file_path):
 
         return data
 
+
 def solve(input_data):
-    print(input_data)
+    registers = defaultdict(int)
+    op_lookup = {
+        "inc": 1,
+        "dec": -1,
+    }
+
+    for line in input_data:
+        matches = re.match(
+            r"(\w+) (\w+) (-?\d+) if (\w+) (<=|>=|==|!=|<|>) (-?\d+)", line
+        )
+        if matches:
+            (
+                reg,
+                op,
+                val,
+                condition_reg,
+                condition,
+                condition_val,
+            ) = matches.groups()
+            val = int(val)
+            condition_val = int(condition_val)
+
+            match condition:
+                case "<":
+                    if registers[condition_reg] < condition_val:
+                        registers[reg] += val * op_lookup[op]
+
+                case ">":
+                    if registers[condition_reg] > condition_val:
+                        registers[reg] += val * op_lookup[op]
+
+                case "<=":
+                    if registers[condition_reg] <= condition_val:
+                        registers[reg] += val * op_lookup[op]
+                #
+                case ">=":
+                    if registers[condition_reg] >= condition_val:
+                        registers[reg] += val * op_lookup[op]
+
+                case "!=":
+                    if registers[condition_reg] != condition_val:
+                        registers[reg] += val * op_lookup[op]
+
+                case "==":
+                    if registers[condition_reg] == condition_val:
+                        registers[reg] += val * op_lookup[op]
+
+    print(registers)
+    return max([v for v in registers.values()])
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    input_path = os.path.join(script_dir, 'input.txt')
-    # input_path = os.path.join(script_dir, 'sample_input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
+    # input_path = os.path.join(script_dir, "sample_input.txt")
 
     # Parse input
     parsed_input = parse_input(input_path)
@@ -36,5 +88,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 08, Part One: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
