44f4e697037d646dbefb273582bf0a023f9804db
Skyrmo <owaingskyrme@gmail.com>
2026-01-19 20:35:45 +0800

    Completed Day 9, parts 1 & 2.

--- day_09/day_09_part_1.py
+++ day_09/day_09_part_1.py
@@ -1,9 +1,9 @@
-
 import os
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
@@ -18,15 +18,61 @@ def parse_input(file_path):
 
         return data
 
-def solve(input_data):
-    print(input_data)
+
+"""
+{}, score of 1.
+{{{}}}, score of 1 + 2 + 3 = 6.
+{{},{}}, score of 1 + 2 + 2 = 5.
+{{{},{},{{}}}}, score of 1 + 2 + 3 + 3 + 3 + 4 = 16.
+{<a>,<a>,<a>,<a>}, score of 1.
+{{<ab>},{<ab>},{<ab>},{<ab>}}, score of 1 + 2 + 2 + 2 + 2 = 9.
+{{<!!>},{<!!>},{<!!>},{<!!>}}, score of 1 + 2 + 2 + 2 + 2 = 9.
+{{<a!>},{<a!>},{<a!>},{<ab>}}, score of 1 + 2 = 3.
+"""
+
+
+def solve(data):
+    n = len(data)
+    i = 0
+    result = 0
+
+    stack = []
+    ignore = False
+
+    while i < n:
+        char = data[i]
+
+        if char == "!":
+            i += 1
+
+        elif char == ">":
+            ignore = False
+
+        elif ignore:
+            i += 1
+            continue
+
+        elif char == "<":
+            ignore = True
+
+        elif char == "{":
+            stack.append(char)
+
+        elif char == "}":
+            result += len(stack)
+            stack.pop()
+
+        i += 1
+
+    return result
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    input_path = os.path.join(script_dir, 'input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
     # input_path = os.path.join(script_dir, 'sample_input.txt')
 
     # Parse input
@@ -36,5 +82,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 09, Part One: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
